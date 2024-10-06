4f464ab846ce8ec4863a29c3dd54973bf911f233
Eddison <jose.pinedo@utec.edu.pe>
2024-10-06 07:41:12 -0500

    Agregar implementación inicial para verificar intersecciones en segmentos de línea

---
+++ exercise07/solution.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// implement a function that decides if there are intersections in a set of line segments, with runtime proportional to N log N (where N is the number of segments in the set)
+
+
