6be224dfa60b6770e1c7952d41f4c990c43c840a
Eddison <jose.pinedo@utec.edu.pe>
2024-10-11 22:31:18 -0500

    Actualizar funciones de intersección en segmentos de línea y nombres de estructuras y funciones en test.cpp y solution.cpp

--- exercise14/solution.cpp
+++ exercise14/solution.cpp
@@ -1,77 +1,81 @@
 #include <iostream>
-#include <opencv2/opencv.hpp>
 #include <vector>
 #include <cmath>
-#include <set>
-#include <tuple>
+#include <algorithm>
+#include <limits>
 
 using namespace std;
 
-// Funcion para calcular el área de un triángulo dado por sus vértices
-float areaTriangulo(const vector<float>& p1, const vector<float>& p2, const vector<float>& p3) {
-    return fabs(p1[0] * (p2[1] - p3[1]) +
-                p2[0] * (p3[1] - p1[1]) +
-                p3[0] * (p1[1] - p2[1])) / 2.0;
+template <typename T>
+struct Triangle {
+    int idx;
+    T area; 
+    T max_angle; 
+};
+
+template <typename T>
+T triangle_area(const vector<T>& p1, const vector<T>& p2, const vector<T>& p3) {
+    return abs(p1[0] * (p2[1] - p3[1]) + p2[0] * (p3[1] - p1[1]) + p3[0] * (p1[1] - p2[1])) / static_cast<T>(2);
 }
 
-// Visvalingam-Whyatt
-vector<vector<float>> visvalingamWhyatt(const vector<vector<float>>& puntos, size_t k) {
-    if (puntos.size() <= k) {
-        return puntos;
+template <typename T>
+T distance(const vector<T>& p1, const vector<T>& p2) {
+    return sqrt(pow(p1[0] - p2[0], 2) + pow(p1[1] - p2[1], 2));
+}
+
+template <typename T>
+T max_triangle_angle(const vector<T>& p1, const vector<T>& p2, const vector<T>& p3) {
+    T a = distance(p2, p3);
+    T b = distance(p1, p3);
+    T c = distance(p1, p2);
+
+    T angle1 = acos((b * b + c * c - a * a) / (2 * b * c));
+    T angle2 = acos((a * a + c * c - b * b) / (2 * a * c));
+    T angle3 = acos((a * a + b * b - c * c) / (2 * a * b));
+
+    return max({angle1, angle2, angle3});
+}
+
+template <typename T>
+vector<vector<T>> triangles_based_simplification(vector<vector<T>> const& vertices, int N) {
+    int original_size = vertices.size();
+    if (N >= original_size) return vertices; // No simplificar si N es mayor o igual al número de vértices
+
+    vector<vector<T>> simplified_vertices = vertices;
+
+    vector<Triangle<T>> triangles;
+    for (int i = 1; i < simplified_vertices.size() - 1; ++i) {
+        T area = triangle_area(simplified_vertices[i - 1], simplified_vertices[i], simplified_vertices[i + 1]);
+        T max_angle = max_triangle_angle(simplified_vertices[i - 1], simplified_vertices[i], simplified_vertices[i + 1]);
+        triangles.push_back({i, area, max_angle});
     }
 
-    // Un vector para almacenar las áreas de los triángulos
-    vector<float> areas(puntos.size(), numeric_limits<float>::max());
+    while (simplified_vertices.size() > N) {
+        sort(triangles.begin(), triangles.end(), [](const Triangle<T>& t1, const Triangle<T>& t2) {
+            if (t1.area == t2.area) {
+                return t1.max_angle > t2.max_angle;
+            }
+            return t1.area < t2.area;
+        });
 
-    // Set para almacenar las áreas de los triángulos
-    set<pair<float, size_t>> areaSet;
+        // Eliminar el vértice central del triángulo más pequeño
+        int to_remove = triangles.front().idx;
+        simplified_vertices.erase(simplified_vertices.begin() + to_remove);
 
-    // Calcular las áreas de los triángulos
-    for (size_t i = 1; i < puntos.size() - 1; ++i) {
-        float area = areaTriangulo(puntos[i - 1], puntos[i], puntos[i + 1]);
-        areas[i] = area;
-        areaSet.insert({area, i});
-    }
-
-    // Un vector para marcar los puntos que se han eliminado
-    vector<bool> puntosActu(puntos.size(), true);
-    size_t numPuntos = puntos.size();
-
-    // Remover los puntos hasta que queden k puntos
-    while (numPuntos > k) {
-        // Obtener el triángulo con el área mínima
-        auto [minArea, index] = *areaSet.begin();
-        areaSet.erase(areaSet.begin());
-        puntosActu[index] = false;
-        numPuntos--;
-
-        // Recalcular las áreas de los triángulos adyacentes
-        if (index > 1 && puntosActu[index - 1] && puntosActu[index + 1]) {
-            areaSet.erase({areas[index - 1], index - 1});
-            areas[index - 1] = areaTriangulo(puntos[index - 2], puntos[index - 1], puntos[index + 1]);
-            areaSet.insert({areas[index - 1], index - 1});
+        // Actualizar los triángulos adyacentes
+        triangles.clear();
+        for (int i = 1; i < simplified_vertices.size() - 1; ++i) {
+            T area = triangle_area(simplified_vertices[i - 1], simplified_vertices[i], simplified_vertices[i + 1]);
+            T max_angle = max_triangle_angle(simplified_vertices[i - 1], simplified_vertices[i], simplified_vertices[i + 1]);
+            triangles.push_back({i, area, max_angle});
         }
-
-        if (index < puntos.size() - 2 && puntosActu[index + 1] && puntosActu[index - 1]) {
-            areaSet.erase({areas[index + 1], index + 1});
-            areas[index + 1] = areaTriangulo(puntos[index - 1], puntos[index + 1], puntos[index + 2]);
-            areaSet.insert({areas[index + 1], index + 1});
-        }
     }
 
-    // Construir el vector de puntos simplificados
-    vector<vector<float>> simplifiedPoints;
-    for (size_t i = 0; i < puntos.size(); ++i) {
-        if (puntosActu[i]) {
-            simplifiedPoints.push_back(puntos[i]);
-        }
-    }
-
-    return simplifiedPoints;
+    return simplified_vertices;
 }
 
 int main() {
-    vector<vector<float>> puntos = {
+    vector<vector<double>> puntos = {
         {0.0, 0.0},
         {1.0, 1.0},    
         {2.0, 2.0},     
@@ -89,58 +93,12 @@ int main() {
         {14.0, 0.0}
     };
 
-    size_t k = 5;
+    int k = 5;
 
-    vector<vector<float>> simplified_points = visvalingamWhyatt(puntos, k);
+    vector<vector<double>> simplified_points = triangles_based_simplification(puntos, k);
 
-    cout << "Simplified puntos:\n";
-    for (const auto& point : simplified_points) {
-        cout << "(" << point[0] << ", " << point[1] << ")\n";
-    }
-
-    // float minX = numeric_limits<float>::max(), minY = numeric_limits<float>::max();
-    // float maxX = numeric_limits<float>::lowest(), maxY = numeric_limits<float>::lowest();
-
-    // for (const auto& point : puntos) {
-    //     if (point[0] < minX) minX = point[0];
-    //     if (point[1] < minY) minY = point[1];
-    //     if (point[0] > maxX) maxX = point[0];
-    //     if (point[1] > maxY) maxY = point[1];
-    // }
-
-    // int margin = 50;
-    // float scaleX = (1000.0f - 2 * margin) / (maxX - minX);
-    // float scaleY = (1000.0f - 2 * margin) / (maxY - minY);
-    // float scale = min(scaleX, scaleY);
-
-    // // Create an image with a margin
-    // cv::Mat image(1000 + 2 * margin, 1000 + 2 * margin, CV_8UC3, cv::Scalar(255, 255, 255));
-
-    // // Draw original puntos in blue
-    // for (const auto& point : puntos) {
-    //     cv::circle(image, cv::Point((point[0] - minX) * scale + margin, 1000 - (point[1] - minY) * scale + margin), 5, cv::Scalar(255, 0, 0), -1);
-    // }
-
-    // // Draw simplified puntos in red
-    // for (const auto& point : simplified_points) {
-    //     cv::circle(image, cv::Point((point[0] - minX) * scale + margin, 1000 - (point[1] - minY) * scale + margin), 5, cv::Scalar(0, 0, 255), -1);
-    // }
-
-    // // Draw lines between original puntos
-    // for (size_t i = 0; i < puntos.size() - 1; ++i) {
-    //     cv::line(image, cv::Point((puntos[i][0] - minX) * scale + margin, 1000 - (puntos[i][1] - minY) * scale + margin), 
-    //             cv::Point((puntos[i + 1][0] - minX) * scale + margin, 1000 - (puntos[i + 1][1] - minY) * scale + margin), 
-    //             cv::Scalar(255, 0, 0), 1);
-    // }
-
-    // // Draw lines between simplified puntos
-    // for (size_t i = 0; i < simplified_points.size() - 1; ++i) {
-    //     cv::line(image, cv::Point((simplified_points[i][0] - minX) * scale + margin, 1000 - (simplified_points[i][1] - minY) * scale + margin), 
-    //             cv::Point((simplified_points[i + 1][0] - minX) * scale + margin, 1000 - (simplified_points[i + 1][1] - minY) * scale + margin), 
-    //             cv::Scalar(0, 0, 255), 1);
-    // }
-
-    // cv::imwrite("Visvalingam-Whyatt.png", image);
+    cout << "Puntos originales: " << puntos.size() << endl;
+    cout << "Puntos simplificados: " << simplified_points.size() << endl;
 
     return 0;
 }
